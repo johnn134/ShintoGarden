6475e19e674a4b6cfaad284d34838ab7513af6c3
johnn134 <johnn134@gmail.com>
2016-08-22 15:25:49 +0900

    Add initial code and objects for bonsai shrine

---
+++ Assets/Bonsai/Shrine/BonsaiShrine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonsaiShrine : MonoBehaviour {
+
+	int activationStage;
+	int points;
+
+	public int maxPoints = 0;
+
+	bool activated;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void addPoints(int p) {
+		points += p;
+	}
+}
